c2b85eecdf2a7d648d6e3b6b8a3a44fff8925c51
yamatoto <mftb35ymt@yahoo.co.jp>
2023-01-15 16:13:59 +0900

    lint-stagedテスト

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import "./globals.css";
 
-export default function RootLayout({ children }: { children }) {
+export default function RootLayout({
+  children,
+}: {
+  children; //: React.ReactNode;
+}) {
   return (
     <html lang="en">
       {/*
